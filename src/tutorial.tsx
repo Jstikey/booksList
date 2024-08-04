70fde70745fd9c006c7416425d370fba242d6c5d
George Joshua <agbanejoshua1@gmail.com>
2024-08-04 19:36:36 +0100

    made and emage slider project

--- src/tutorial.tsx
+++ src/tutorial.tsx
@@ -1,52 +1,28 @@
-import { useEffect, useState } from "react";
+import { people } from "./firstComponent";
 
-interface NasaProps {
-  explanation: string;
-  hdurl: string;
-  title: string;
-  url: string;
-}
 export const FetchData = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [nasa, setNasa] = useState<NasaProps | null>(null);
-
-  const getData = async () => {
-    try {
-      const resp = await fetch(
-        "https://api.nasa.gov/planetary/apod?api_key=99Oi8bhW77Bf4EnjoaAhnmfJe5Za606gskWfiXAg"
-      );
-
-      if (!resp.ok) {
-        setIsError(true);
-        setIsLoading(false);
-        return;
-      }
-      // const { explanation, hdurl, title, url } = await resp.json();
-      const data = await resp.json();
-      setNasa(data);
-    } catch (error) {
-      console.log(error);
-    }
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getData();
-  }, []);
-  if (isLoading) return <h1>Loading...</h1>;
-  if (isError) return <h1>there was an error loading this page</h1>;
-  if (!nasa) return;
-  const { explanation, hdurl, url, title } = nasa;
   return (
-    <div>
-      <h2>{title}</h2>
-      <img
-        style={{ objectFit: "cover", width: "200px", height: "250px" }}
-        src={hdurl}
-        alt=""
-      />
-      <p>{explanation}</p>
-      <a href={url}>go there</a>
-    </div>
+    <>
+      {people.map((person) => (
+        <div
+          style={{
+            border: "1px solid grey",
+            borderRadius: "0.5rem",
+            marginBottom: "1rem",
+          }}
+          key={person.id}
+        >
+          <h2>{person.name}</h2>
+          {person.images?.map((img) => (
+            <img
+              style={{ width: "120px", height: "150px", objectFit: "cover" }}
+              src={img.small.url}
+              alt=""
+            />
+          ))}
+          <h4>{person.nickName}</h4>
+        </div>
+      ))}
+    </>
   );
 };
